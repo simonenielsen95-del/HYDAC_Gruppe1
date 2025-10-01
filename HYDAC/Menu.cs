2c5c0ee1d459e8b01642bc9701ca0f90117b14a6
Simone Nielsen <simonenielsen95@gmail.com>
2025-10-01 12:26:54 +0200

    nyeste nye

--- HYDAC/Menu.cs
+++ HYDAC/Menu.cs
@@ -12,17 +12,12 @@ namespace HYDAC
     internal class Menu //stilesering?
 
     {
-        
+
         public string Title;
         public MenuItem[] MenuItems;
         public int ItemCount = 0;
 
         Gæst[] gæster = new Gæst[10]; // array der kan indeholde class Gæst
-
-
-
-        public int[] Nr = new int[10]; // til test for at printe hvad nr personen er blevet skrevet ind som 
-        int nr = 0; // holder for at kontrolere index
 
         public void Show()
         {
@@ -32,7 +27,6 @@ namespace HYDAC
             for (int i = 0; i < ItemCount; i++)
             {
                 Console.WriteLine("   " + MenuItems[i].Title);
-
             }
             Console.WriteLine();
             Console.WriteLine("(Tryk menupunkt eller 0 for at afslutte)");
@@ -42,110 +36,125 @@ namespace HYDAC
             if (MI == "1")
             {
 
-                
-                gæster[nr] = new Gæst(); // indsætter en ny gæst ind på et index af Gæst arrayet
-
-                if (nr > 9) // tjekker om der kan være flere gæster i array
-                {
-                    Console.WriteLine("der er ikke plads til flere gæster");
-                    Console.WriteLine("tast enter for at gå tilbage til menu");
-                    Console.ReadLine();
-                    Show();
-                }
                 Console.WriteLine("Intast navn:");
-                gæster[nr].GuestName = Console.ReadLine();  // gemmer det indtastede navn 
+                string guestName = Console.ReadLine();  // gemmer det indtastede navn 
 
+                Console.Clear();
                 Console.WriteLine("Intast virksomhed:");
-                gæster[nr].CompanyName = Console.ReadLine(); // gemmer det indtastede virksomhed
+                string companyName = Console.ReadLine(); // gemmer det indtastede virksomhed
 
+                Console.Clear();
                 Console.WriteLine("Indtast dato for ankomst:");
-                gæster[nr].Date = Console.ReadLine(); // gemmer det indtastede dato
+                string date = Console.ReadLine(); // gemmer det indtastede dato
 
+                Console.Clear();
                 Console.WriteLine("Indtast klokkeslæt for ankomst:");
-                gæster[nr].Arrival = Console.ReadLine(); // gemmer den indtastede ankomst tid
+                string arival = Console.ReadLine(); // gemmer den indtastede ankomst tid
 
+                Console.Clear();
                 Console.WriteLine("skriv gem for at gemme"); // lige nu gemmes gæsten uanset hvad der bliver indtastet her
                 string gem = Console.ReadLine();
 
                 if (gem.ToLower() == "gem")
                 {
+                    bool ableToSave = false;
+                    for (int i = 0; i < gæster.Length; i++)
+                    {
+                        if (gæster[i] == null)
+                        {
+                            gæster[i] = new Gæst();
+                            ableToSave = true;
+                            gæster[i].GuestName = guestName;
+                            gæster[i].CompanyName = companyName;
+                            gæster[i].Date = date;
+                            gæster[i].Arrival = arival;
 
-                    nr++; // tæller en op så den næste gæst kan blive skrevet ind på en ny plads.
-
+                            Console.Clear();
+                            Console.WriteLine("Gæsten er gemt i gæstebogen");
+                            Console.WriteLine("Tryk enter for at gå tilbage til menu");
+                            Console.ReadLine();
+                            Show();
+                        }
+                    }
+                    if (!ableToSave)
+                    {
+                        Console.WriteLine("der er ikke plads til flere gæster");
+                        Console.WriteLine("tast enter for at gå tilbage til menu");
+                        Console.ReadLine();
+                        Show();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("intastede oplysninger er ikke gent i gæste bogen");
                     Console.WriteLine("Tryk enter for at gå tilbage til menu");
                     Console.ReadLine();
                     Show();
-
                 }
-
-                else
-                    Console.WriteLine("intastede oplysninger er ikke gent i gæste bogen");
-                Console.WriteLine("Tryk enter for at gå tilbage til menu");
-                Console.ReadLine();
-                Show();
             }
 
             if (MI == "2") // se i gæstebogen (printer alle gæsterne ud)
             {
                 Console.WriteLine(" Gæstebog:");
                 Console.WriteLine("-------------------------------------");
-                for (int i = 0; i < nr; i++)
+                for (int i = 0; i < gæster.Length; i++)
                 {
-
-                    Console.WriteLine(
+                    if (gæster[i] != null)
+                    {
+                        Console.WriteLine(
                         " \n" +
                         $" Gæstens navn er {gæster[i].GuestName}\n " +
                         $"Virksomheden er {gæster[i].CompanyName}\n " +
                         $"Din ankomst dato er {gæster[i].Date}\n " +
                         $"Din akomst tid er {gæster[i].Arrival}\n " +
-                        $"Du er gæst nr {i + 1}\n"
+                        $"{i}"
                         );
-                    Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("-------------------------------------");
+                    }
                 }
                 Console.ReadLine();
                 Show();
             }
-            if (MI == "3") // forsøg på at slette gæsten igen
+
+            if (MI == "3") // forsøg på at slette gæsten igen (det virker hvis gæsten ER i gæstebogen ellers crasher det)
             {
                 Console.WriteLine("Slet Gæst");
-
                 Console.WriteLine("indtast navn:");
-
-                bool foundName = false;
-
                 string name = Console.ReadLine();
+                bool foundName = false;
+                Console.Clear();
+               
                 while (!foundName)
-                    for (int i = 0; i < nr; i++)
+                {
+                    for (int i = 0; i < gæster.Length; i++)
                     {
-                        if (gæster[i].GuestName == name)
+                        if (gæster[i]!=null && gæster[i].GuestName == name)
                         {
-                            foundName = true; break;
+                            foundName = true;
+                            gæster[i] = null;
+
+
+                            Console.WriteLine("gæsten er nu slettet fra gæstebogen.");
+                            Console.WriteLine("tast enter for at komme tilbage til menuen.");
+                            Console.ReadLine();
+                            Show();
                         }
-                        else
-                        {
-
-                        }
-
-
-
-
-
-                       
-                        Console.ReadLine();
-
-
-
-
                     }
-
-
-
-
+                }
+                if (!foundName)
+                {
+                    Console.WriteLine("gæsten kunne ikke findes i gæstebogen");
+                    Console.WriteLine("tast enter for at komme tilbage til menuen.");
+                    Console.ReadLine();
+                    Show();
+                }
             }
-
-
-
-
+            if (MI=="0")
+            {
+                return;
+            }
         }
     }
 }
+    
+
